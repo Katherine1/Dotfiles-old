3fabedf9f6e0c95ace32d800d07a92a036751400
Emily Marriott <emilylynmarriott@proton.me>
2023-09-22 11:24:03 -0500

    Game settings updates

---
+++ .config/game-settings/cyberpunk.sh
@@ -0,0 +1,4 @@
+#!/bin/env zsh
+
+source ~/.config/game-settings/ray_tracing.sh
+export DXVK_NVAPI_DRIVER_VERSION=53742
--- .config/game-settings/ray_tracing.sh
+++ .config/game-settings/ray_tracing.sh
@@ -1,5 +1,5 @@
 #!/bin/env zsh
 
-source expose-nvidia.sh
+source ~/.config/game-settings/expose-nvidia.sh
 export VKD3D_CONFIG=dxr,dxr11
 export PROTON_ENABLE_NGX_UPDATER=1
