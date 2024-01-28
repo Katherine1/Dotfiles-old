e0ff825e7590432e4f64bdd2e617cd8180a303f8
Emily Marriott <emilylynmarriott@proton.me>
2024-01-27 20:46:50 -0600

    Add tmux config and restyle neovim

--- .config/nvim/after/plugin/theme.lua
+++ .config/nvim/after/plugin/theme.lua
@@ -1,3 +1,42 @@
-vim.cmd.colorscheme("gruvbox-material")
+require("catppuccin").setup({
+    integrations = {
+        treesitter = true,
+        mason = false,
+        cmp = true,
+        telescope = {
+            enabled = true
+        },
+        native_lsp = {
+            enabled = true,
+            virtual_text = {
+                errors = {"italic"},
+                hints = {"italic"},
+                warnings = {"italic"},
+                information = {"italic"}
+            },
+            underlines = {
+                errors = { "underline" },
+                hints = { "underline" },
+                warnings = { "underline" },
+                information = { "underline" }
+            },
+            inlay_hints = {
+                background = true
+            }
+        }
+    }
+})
+
+local ctp_feline = require('catppuccin.groups.integrations.feline')
+
+--ctp_feline.setup() -- breaks things, don't know why
+
+require("feline").setup({
+    components = ctp_feline.get(),
+})
+
+require("feline").winbar.setup()
+
+vim.cmd.colorscheme("catppuccin")
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -16,6 +16,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
 	{'nvim-telescope/telescope.nvim', branch= '0.1.x', dependencies = {'nvim-lua/plenary.nvim'}},
 	'sainnhe/gruvbox-material',
+    {'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
 	{'nvim-treesitter/nvim-treesitter', run = {':TSUpdate'}},
 	'mbbill/undotree',
 	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x', lazy = true, config = false},
@@ -23,6 +24,8 @@ require("lazy").setup({
 	{'hrsh7th/nvim-cmp', dependencies = {'L3MON4D3/LuaSnip'}},
 	'williamboman/mason.nvim',
 	{'williamboman/mason-lspconfig.nvim', dependencies = {'williamboman/mason.nvim', 'neovim/nvim-lspconfig'}},
-    'vim-airline/vim-airline'
+--    'vim-airline/vim-airline',
+    {'christoomey/vim-tmux-navigator', lazy = false},
+    'feline-nvim/feline.nvim'
 })
 
