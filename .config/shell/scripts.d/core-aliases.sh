8eb0b84e4e8a27aeb03023cf3347135c5886d942
Emily Marriott <emilylynmarriott@proton.me>
2024-01-28 17:29:42 -0600

    Gate core aliases behind checks for the commands they use

--- .config/shell/scripts.d/core-aliases.sh
+++ .config/shell/scripts.d/core-aliases.sh
@@ -1,10 +1,19 @@
 #!/bin/env sh
 
-alias ls="eza --icons --group-directories-first"
-alias cat="bat"
+if command -v eza > /dev/null 2>&1; then
+    alias ls="eza --icons --group-directories-first"
+elif command -v exa > /dev/null 2>&1; then
+    alias ls="exa --icons --group-directories-first"
+fi
+
+if command -v bat > /dev/null 2>&1; then
+    alias cat="bat"
+    if command -v fzf > /dev/null 2>&1; then
+        alias fzf='fzf --preview "bat --color=always --style=numbers --line-range=:500 {}"'
+    fi
+fi
+
 alias grep='grep --color=always'
 alias ip="ip -color"
-alias fzf='fzf --preview "bat --color=always --style=numbers --line-range=:500 {}"'
-
 
 alias config="/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
