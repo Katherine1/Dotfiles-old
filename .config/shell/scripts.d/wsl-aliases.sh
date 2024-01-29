22a44095c23f21a13d4fa42d523bd9066f0a8f63
Emily Marriott <emilylynmarriott@proton.me>
2024-01-29 06:20:06 -0600

    Change WSL detection method

--- .config/shell/scripts.d/wsl-aliases.sh
+++ .config/shell/scripts.d/wsl-aliases.sh
@@ -1,5 +1,7 @@
-if [ -n $WSL_DISTRO_NAME ]; then
-    function notify-send() {
+#!/bin/env sh
+
+if [ -f "/proc/sys/fs/binfmt_misc/WSLInterop" ]; then
+    notify-send() {
         wsl-notify-send.exe --category $WSL_DISTRO_NAME "${@}";
     }
 fi
